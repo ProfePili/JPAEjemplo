75edb5f556d4f87c107ae6689b5dc4bb6932b096
ProfePili <pilar.gonzalez@eggcooperation.com>
2022-03-16 18:00:00 -0300

    modificacion

--- src/jpaejemplo/persistencia/UsuarioDAO.java
+++ src/jpaejemplo/persistencia/UsuarioDAO.java
@@ -1,29 +1,24 @@
-
-
 package jpaejemplo.persistencia;
 
 import java.util.List;
 import jpaejemplo.entidades.Usuario;
 
+public class UsuarioDAO extends DAO<Usuario> {
 
-public class UsuarioDAO extends DAO<Usuario>{
-    
-    public void guardarUsuario(Usuario usuario){
+    public void guardarUsuario(Usuario usuario) {
         super.guardar(usuario);
     }
-    
-    public Usuario buscarUsuarioPorEmail(String email){
-        conectar();
+
+    public Usuario buscarUsuarioPorEmail(String email) {
         Usuario usuario = em.find(Usuario.class, email);
-        desconectar();
         return usuario;
     }
-    
-    public List<Usuario> buscarUsuarioPorRangoEdad(int edadMenor, int edadMayor){
-        conectar();
+
+    public List<Usuario> buscarUsuarioPorRangoEdad(int edadMenor, int edadMayor) {
+
         List<Usuario> usuarios = em.createQuery("SELECT u FROM Usuario u WHERE u.edad BETWEEN :edad1 AND :edad2")
                 .setParameter("edad1", edadMenor).setParameter("edad2", edadMayor).getResultList();
-        desconectar();
+
         return usuarios;
     }
 }
